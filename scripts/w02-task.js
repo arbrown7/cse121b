55dd097654ee6de1b25f7368069f6539966a8c03
arbrown7 <arbrown7@byui.edu>
2023-09-20 13:22:35 -0600

    sep 20

---
+++ scripts/w02-task.js
@@ -0,0 +1,29 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Autumn Brown';
+let currentYear = 2023;
+let profilePicture = 'images/DSC06116.JPG';
+
+/* Step 3 - Element Variables */
+const nameElement =  document.getElementById('name');
+const foodElement =  document.getElementById('food');
+const yearElement =  document.querySelector('#year');
+const imageElement = document.getElementsByTagName('img');
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
